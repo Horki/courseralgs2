2cd0447882e4e3aff9bf7cd8e0bc29a0b437b6a2
Horki <ivanzvonimirhorvat@gmail.com>
2020-06-08 22:52:07 +0200

    init fixes

---
+++ src/sedgewick/coursera/week1/Test.java
@@ -0,0 +1,9 @@
+package sedgewick.coursera.week1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Test {
+    public static void main(String[] args) {
+        StdOut.println("init");
+    }
+}
---
+++ src/sedgewick/coursera/week2/NextWeek.java
@@ -0,0 +1,9 @@
+package sedgewick.coursera.week2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class NextWeek {
+    public static void main(String[] args) {
+        StdOut.println("next week!");
+    }
+}
