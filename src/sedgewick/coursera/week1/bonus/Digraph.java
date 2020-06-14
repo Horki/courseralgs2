94f3d9ad82ea672d81671e1e90753e958827ce81
Horki <ivanzvonimirhorvat@gmail.com>
2020-06-14 20:55:32 +0200

    week1: digraph typo; directed dfs

--- src/sedgewick/coursera/week1/bonus/Digraph.java
+++ src/sedgewick/coursera/week1/bonus/Digraph.java
@@ -4,21 +4,21 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import sedgewick.coursera.week1.abstracts.Adj;
 
-public class Diagraph extends Adj {
+public class Digraph extends Adj {
 
     // create an empty digraph with V vertices
-    public Diagraph(int V) {
+    public Digraph(int V) {
         super(V);
     }
 
     // create a digraph from input stream
-    public Diagraph(In in) {
+    public Digraph(In in) {
         super(in);
     }
 
     // reverse of this digraph
-    public Diagraph reverse() {
-        Diagraph reverse = new Diagraph(V);
+    public Digraph reverse() {
+        Digraph reverse = new Digraph(V);
         for (int v = 0; v < V; ++v) {
             for (int w : adj(v)) {
                 reverse.addEdge(w, v);
@@ -49,9 +49,9 @@ public class Diagraph extends Adj {
     // tinyDG.txt
     public static void main(String[] args) {
         In in = new In(args[0]);
-        Diagraph G = new Diagraph(in);
+        Digraph G = new Digraph(in);
         StdOut.println(G);
-        Diagraph reverse = G.reverse();
+        Digraph reverse = G.reverse();
         StdOut.println("Reversed:");
         StdOut.println(reverse);
     }
---
+++ src/sedgewick/coursera/week1/bonus/DirectedDFS.java
@@ -0,0 +1,47 @@
+package sedgewick.coursera.week1.bonus;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import sedgewick.coursera.week1.abstracts.Adj;
+
+public class DirectedDFS {
+    private boolean[] marked;
+
+    public DirectedDFS(Adj G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        for (int s : sources) {
+            dfs(G, s);
+        }
+    }
+
+    private void dfs(Adj G, int s) {
+        marked[s] = true;
+        for (int w : G.adj(s)) {
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean visited(int v) {
+        return marked[v];
+    }
+
+    // tinyDG.txt
+    public static void main(String[] args) {
+        Adj G = new Digraph(new In(args[0]));
+        Bag<Integer> sources = new Bag<>();
+        for (int i = 1; i < args.length; ++i) {
+            sources.add(Integer.parseInt(args[i]));
+        }
+        DirectedDFS reachable = new DirectedDFS(G, sources);
+        for (int v = 0; v < G.V(); ++v) {
+            if (reachable.visited(v)) {
+                StdOut.print(v + " ");
+            }
+        }
+        StdOut.println();
+    }
+
+}
