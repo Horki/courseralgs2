72cfc1bcd94f858832923a9a56fd5b95fa47c4aa
Horki <ivanzvonimirhorvat@gmail.com>
2020-06-17 18:52:54 +0200

    week2: using acyclis; with algs4.topology search

---
+++ src/sedgewick/coursera/week2/bonus/AcyclicSP.java
@@ -0,0 +1,78 @@
+package sedgewick.coursera.week2.bonus;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.Topological;
+import sedgewick.coursera.week2.bonus.abstracts.SingleSourceShortestPath;
+
+public class AcyclicSP extends SingleSourceShortestPath {
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+
+    public AcyclicSP(EdgeWeightedDigraph G, int source) {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+        for (int v = 0; v < G.V(); ++v) {
+            distTo[v] = Double.POSITIVE_INFINITY;
+        }
+        distTo[source] = 0.0;
+        Digraph digraph = G.toDigraph(); // dirty fix
+        Topological top = new Topological(digraph);
+        for (int v : top.order()) {
+            for (DirectedEdge e : G.adj(v)) {
+                relax(e);
+            }
+        }
+    }
+
+    private void relax(DirectedEdge e) {
+        int v = e.from();
+        int w = e.to();
+        if (distTo[w] > distTo[v] + e.weight()) {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
+        }
+    }
+
+    @Override
+    public double distTo(int v) {
+        return distTo[v];
+    }
+
+    @Override
+    public Iterable<DirectedEdge> pathTo(int v) {
+        if (!hasPathTo(v)) {
+            return null;
+        }
+        Stack<DirectedEdge> path = new Stack<>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+            path.push(e);
+        }
+        return path;
+    }
+
+    @Override
+    public boolean hasPathTo(int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    // tinyEWDAG.txt
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+        int source = 5;
+        AcyclicSP sp = new AcyclicSP(G, source);
+        for (int v = 0; v < G.V(); ++v) {
+            if (source != v && sp.hasPathTo(v)) {
+                StdOut.printf("%d to %d (%.2f): ", source, v, sp.distTo(v));
+                for (DirectedEdge e : sp.pathTo(v)) {
+                    StdOut.print(e + " ");
+                }
+                StdOut.println();
+            }
+        }
+    }
+
+}
