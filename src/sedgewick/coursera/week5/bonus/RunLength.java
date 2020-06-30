e465058d3d4cc7208c205c3c1983cb01535fae83
Horki <ivanzvonimirhorvat@gmail.com>
2020-06-30 15:07:54 +0200

    week5: bonus; compres+decompress run length

---
+++ src/sedgewick/coursera/week5/bonus/RunLength.java
@@ -0,0 +1,57 @@
+package sedgewick.coursera.week5.bonus;
+
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+public class RunLength {
+    // maximum run-length count
+    private final static int R = 256;
+    // numbers of bits per count
+    private final static int lgR = 8;
+
+    private static void compress() {
+        char cnt = 0;
+        boolean b, old = false;
+        while (!BinaryStdIn.isEmpty()) {
+            b = BinaryStdIn.readBoolean();
+            if (b != old) {
+                BinaryStdOut.write(cnt);
+                cnt = 0;
+                old = !old;
+            } else {
+                if (cnt == R - 1) {
+                    BinaryStdOut.write(cnt);
+                    cnt = 0;
+                    BinaryStdOut.write(cnt);
+                }
+            }
+            ++cnt;
+        }
+        BinaryStdOut.write(cnt);
+        BinaryStdOut.close();
+    }
+
+    private static void expand() {
+        boolean bit = false;
+        while (!BinaryStdIn.isEmpty()) {
+            int run = BinaryStdIn.readInt(lgR);
+            for (int i = 0; i < run; ++i) {
+                BinaryStdOut.write(bit);
+            }
+            bit = !bit;
+        }
+        BinaryStdOut.close();
+    }
+
+    // - > abra.txt
+    // + > abra.txt
+    public static void main(String[] args) {
+        if (args[0].equals("-")) {
+            compress();
+        } else if (args[0].equals("+")) {
+            expand();
+        } else {
+            throw new IllegalArgumentException("Illegal command line argument");
+        }
+    }
+}
