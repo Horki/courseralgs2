6c27307d46bb2dce8c64827cacc755f0c651758b
Horki <ivanzvonimirhorvat@gmail.com>
2020-07-01 16:52:12 +0200

    week6: init

---
+++ src/sedgewick/coursera/week6/bonus_1_linear/Linear.java
@@ -0,0 +1,9 @@
+package sedgewick.coursera.week6.bonus_1_linear;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Linear {
+    public static void main(String[] args) {
+        StdOut.println("bonus 1; linear programming");
+    }
+}
---
+++ src/sedgewick/coursera/week6/bonus_2_intractability/Intractability.java
@@ -0,0 +1,9 @@
+package sedgewick.coursera.week6.bonus_2_intractability;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Intractability {
+    public static void main(String[] args) {
+        StdOut.println("Bonus 6: Intractability");
+    }
+}
