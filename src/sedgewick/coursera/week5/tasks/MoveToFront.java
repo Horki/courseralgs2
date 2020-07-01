41a8e7037edeaf6c5bb519a0fd0a16bd1e3fc5fd
Horki <ivanzvonimirhorvat@gmail.com>
2020-07-01 14:25:01 +0200

    week5: task1; Move to front

--- src/sedgewick/coursera/week5/tasks/MoveToFront.java
+++ src/sedgewick/coursera/week5/tasks/MoveToFront.java
@@ -1,19 +1,56 @@
 package sedgewick.coursera.week5.tasks;
 
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
 public class MoveToFront {
+    private MoveToFront() {
+        // Do not instantiate.
+    }
+
+    private static final int R = 256;
 
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
-        // TODO: implement
+        char[] orderedSequence = new char[R];
+        for (int i = 0; i < R; ++i) {
+            orderedSequence[i] = (char) i;
+        }
+        while (!BinaryStdIn.isEmpty()) {
+            char input = BinaryStdIn.readChar();
+            for (int i = 0; i < R; ++i) {
+                if (orderedSequence[i] == input) {
+                    BinaryStdOut.write(orderedSequence[i], 8);
+                    swap(orderedSequence, 0, i);
+                }
+            }
+        }
+        BinaryStdOut.close();
+    }
+
+    private static void swap(char array[], int first, int second) {
+        char temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode() {
-        // TODO: implement
+        char[] orderedSequence = new char[R];
+        for (int i = 0; i < R; ++i) {
+            orderedSequence[i] = (char) i;
+        }
+        while (!BinaryStdIn.isEmpty()) {
+            char input = BinaryStdIn.readChar();
+            BinaryStdOut.write(orderedSequence[input], 8);
+            swap(orderedSequence, 0, input);
+        }
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
+    // sample1.txt
     public static void main(String[] args) {
         if (args[0].equals("-")) {
             encode();
@@ -22,7 +59,5 @@ public class MoveToFront {
         } else {
             throw new IllegalArgumentException("Illegal command line argument");
         }
-
     }
-
 }
